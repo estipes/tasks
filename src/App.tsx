06e3493f6093bd587d5fffef9ebeea060b6b171c
Ethan S <estipes@udel.edu>
2022-02-09 22:39:46 -0500

    Fixed Hello World on App

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,12 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript. Ethan Stipes.
+                Hello World. UD CISC275 with React Hooks and TypeScript. Ethan
+                Stipes.
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload. Hello World
+                automatically reload.
             </p>
         </div>
     );
