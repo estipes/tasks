2494b84b5ca68874ff50a5fae08d44e55a081fe0
Ethan S <estipes@udel.edu>
2022-04-03 19:50:55 -0400

    Fixed Form Components Warnings/Errors

--- src/form-components/GiveAttempts.tsx
+++ src/form-components/GiveAttempts.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Form } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
 
 export function GiveAttempts(): JSX.Element {
     const [attempts, setAttempts] = useState<number>(3);
@@ -13,22 +13,20 @@ export function GiveAttempts(): JSX.Element {
                     value={request}
                     onChange={(e) => setRequest(parseInt(e.target.value))}
                 />
-                <Form.Check
-                    disabled={attempts === 0}
-                    type="switch"
-                    name="use"
-                    onChange={() => setAttempts(attempts - 1)}
-                ></Form.Check>
-                <Form.Check
-                    type="switch"
-                    name="gain"
-                    onChange={() =>
-                        setAttempts(
-                            isNaN(request) ? attempts : attempts + request
-                        )
-                    }
-                ></Form.Check>
             </Form.Group>
+            <Button
+                onClick={() => setAttempts(attempts - 1)}
+                disabled={attempts === 0}
+            >
+                use
+            </Button>
+            <Button
+                onClick={() =>
+                    setAttempts(isNaN(request) ? attempts : attempts + request)
+                }
+            >
+                gain
+            </Button>
         </div>
     );
 }
